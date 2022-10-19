57b56e66938136488ce70e5f7a02294f8840eb82
Julio Cesar Damasceno <45015654+xjulio@users.noreply.github.com>
2022-10-19 02:01:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ sample = Flask(__name__)
 
 @sample.route("/")
 def main():
-    return "hello from container!" 
+    return "Bem vindo ao serviço do container 5050!" 
 
 if __name__ == "__main__":
   sample.run(host="0.0.0.0", port=5050)
